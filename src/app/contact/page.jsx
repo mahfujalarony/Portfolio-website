1c4debe0b0f039f20eafb7375eb3f804f48db30c
mahfujalarony <149530147+mahfujalarony@users.noreply.github.com>
2024-11-13 18:20:11 +0600

    commit 2

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -46,7 +46,7 @@ const contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>  
+              <h3 className="text-4xl text-accent">Let&#39;s work together</h3>  
               <p className="text-white/60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis vitae delectus consectetur rerum ex facilis excepturi nulla! Veritatis, nulla laborum facere aliquam suscipit est cum corporis autem beatae doloremque!</p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
                 <Input type="firstname" placeholder='FirstName'/>
