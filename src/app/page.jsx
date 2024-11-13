38c3eb8cb33072ccbacb1565f709fb8034578008
mahfujalarony <149530147+mahfujalarony@users.noreply.github.com>
2024-11-13 18:09:00 +0600

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,11 +11,9 @@ export default function Home() {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span>Software Developer</span>
-            <h1 className="h1">
-              Hello I'm <br /> <span className="text-accent">Mahfuj Alam Rony</span>
-            </h1>
+            <h1 className="h1">Hello I&#39;m <br /> <span className="text-accent">Mahfuj Alam Rony</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-            Skilled React.js developer with expertise in building high-performance UIs, converting Figma designs into responsive applications using Tailwind CSS, and writing clean, maintainable code.
+            Skilled React js developer with expertise in building high-performance UIs, converting Figma designs into responsive applications using Tailwind CSS, and writing clean, maintainable code.
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8">
